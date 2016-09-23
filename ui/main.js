b93fc319358d758c63a1cb8b30ab5baf299125b7
abiqwerty <abi199729@gmail.com>
2016-09-23 20:42:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+var counter = o;
+button.onclick = function(){
+    counter = counter + 1;
+    var span = documentgetElementById('count');
+    span.innerHTML = counter.toString();
+};
